87ebb753d658b77e516ca0c9f37306b0967e16dd
Ryan <ryan@siliconmtn.com>
2014-09-16 11:30:25 -0600

    adding updates

---
+++ src/MapAndList/MapAndListEnvironment.java
@@ -0,0 +1,38 @@
+package MapAndList;
+
+
+/******************************************************************************
+ * <b>Title</B>: MapAndListEnvironment.java Description: this project will 
+ * include: a map with state names and state keys and a method to order and 
+ * reverse the order.  
+ * 
+ * a list containing a deck of cards. and a method to shuffle them into random 
+ * order.
+ * 
+ * Copyright: Copyright (c) 2014 Company: Silicon Mountain Technologies
+ *
+ * @author: Ryan Riker
+ * @version 1.0
+ * @since 09/10/2014 last update: never
+ *****************************************************************************/
+
+public class MapAndListEnvironment {
+
+	public MapAndListEnvironment() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		MapAndListEnvironment me = new MapAndListEnvironment();
+		me.process();
+		
+		
+	}
+	
+	public void process() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
